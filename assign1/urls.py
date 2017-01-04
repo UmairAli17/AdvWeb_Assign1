ae24cc08282756fdf1fa28d555f1c468fa0574e7
Umair Ali <umairali17@hotmail.co.uk>
2017-01-04 20:08:49 +0000

    first commit - test on VM

--- assign1/urls.py
+++ assign1/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponseRedirect
+
+
 
 
 urlpatterns = [
+    # so the root will direct directly to the shop app
+    url(r'^$', lambda r: HttpResponseRedirect('shop/')),
     url(r'^admin/', admin.site.urls),
     url(r'^shop/', include('shop.urls'))
 ]
--- shop/forms.py
+++ shop/forms.py
@@ -4,11 +4,11 @@ from django import forms
 
 
 class UserRegForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    conf_password = forms.CharField(widget=forms.PasswordInput(), label="Please Confirm Your Password")
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter a Password Here', 'class': 'shop-formField'}))
+    conf_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password Here', 'class': 'shop-formField'}))
 
     # create the below function, this checks the currently submitted data and checks whether the password field matches the
-    # confirm passwoprd field
+    # confirm password field
     def clean_passwords(self):
         if self.cleaned_data['password'] != self.cleaned_data['conf_password']:
             raise forms.ValidationError("Your passwords do not match!")
@@ -19,18 +19,31 @@ class UserRegForm(forms.ModelForm):
         return super(UserRegForm, self).clean()
 
     class Meta:
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Enter A Username Here', 'class': 'shop-formField'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Enter Your Email Here', 'class': 'shop-formField'}),
+        }
         model = User
         fields = ['username', 'email', 'password']
 
 
 class UserLogForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username', 'class': 'shop-formField'}),
+            'password': forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'shop-formField'}),
+        }
         fields = ['username', 'password']
 
 class AddProductForm(forms.ModelForm):
+    product_image = forms.FileField(label="Upload a Product Image", required=True)
     class Meta:
         model = Product
-        fields = ['product_name', 'product_desc', 'product_image']
+        widgets = {
+            'product_name': forms.TextInput(attrs={'placeholder': 'Enter Product Name', 'class': 'shop-formField'}),
+            'product_desc': forms.Textarea(attrs={'placeholder': 'Enter Product Description Here...', 'class': 'shop-formField'}),
+        }
         exclude = ['business']
+
+
